f4f8f3c7d19d8502f8feab4f95eaac7e1a4228ef
FEDERICO <federico.saggiro@icloud.com>
2025-05-08 11:50:23 +0200

    porta

--- server.js
+++ server.js
@@ -213,6 +213,6 @@ app.delete('/delete/utente/:id', async (req, res) => {
 
 const server = http.createServer(app);
 
-server.listen(6300, () => {
+server.listen(6000, () => {
     console.log("- server running");
 });
