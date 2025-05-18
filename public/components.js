cc9c3fb5d09a6aca9ad208924be42ab6eb726b11
diCugno <dialessandro@itis-molinari.eu>
2025-05-18 12:34:29 +0200

    css

--- public/components.js
+++ public/components.js
@@ -186,7 +186,7 @@ export function MostraImmagini(e) {
                         <img src="./../files/${img.image}" class="card-img-top" alt="${img.descrizione || ""}">
                             <div class="card-body">
                                 <p class="card-text">${img.descrizione || ""}</p>
-                                <button class="btn btn-danger btn-elimina" data-id="${img.id}">Elimina</button>
+                                <button class="btn-elimina" data-id="${img.id}">Elimina</button>
                             </div>
                     </div>
                     `;
